bbadf4374ef0320850f8696977fd5a47c23fd8c3
Shodiqul Muzaki <muzaki@users-MacBook-Pro.local>
2021-12-17 13:16:17 +0700

    add quest mining

--- routers/index.js
+++ routers/index.js
@@ -1,8 +1,6 @@
 const express = require('express')
 const app = express.Router()
 const DashboardHandler = require('./dashboard'), Dashboard = new DashboardHandler()
-const HeroesHandler = require('./Heroes'), Heroes = new HeroesHandler()
 
 app.get('/', Dashboard.index)
-app.get('/heroes', Heroes.index)
 module.exports = exports = app
