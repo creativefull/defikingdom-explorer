7ebd67697844b3c5ef388f06d64701e4a31c71c3
Shodiqul Muzaki <muzaki@users-MacBook-Pro.local>
2021-12-17 14:20:47 +0700

    add view history latest quests

--- miners/abis.js
+++ miners/abis.js
@@ -1,13 +1,19 @@
 exports.abis = [{
     'type': 'ROUTER',
     'address': '0x24ad62502d1c652cc7684081169d04896ac20f30',
-    'abi': require('../abi/router.json')
+    'abi': require('../abi/router.json'),
+    'icon': 'fa-arrows-alt-h',
+    'color': '#BE90D4'
 }, {
     'type': 'BANK',
     'address': '0xa9ce83507d872c5e1273e745abcfda849daa654f',
-    'abi': require('../abi/bank.json')
+    'abi': require('../abi/bank.json'),
+    'icon': 'fa-piggy-bank',
+    'color': '#F9BF3B'
 }, {
     'type': 'QUEST',
     'address': '0x5100bd31b822371108a0f63dcfb6594b9919eaf4',
-    'abi': require('../abi/quest.json')
+    'abi': require('../abi/quest.json'),
+    'icon': 'fa-bolt',
+    'color': '#FFA27B'
 }]
