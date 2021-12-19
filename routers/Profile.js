7cb9099edd1b0629ab492be398a1265aef0409bf
Achmad Safiul Ubab <safiulred@gmail.com>
2021-12-19 09:15:09 +0700

    .

--- routers/Profile.js
+++ routers/Profile.js
@@ -1,16 +1,85 @@
-const Web3 = require('web3')
-const web3 = new Web3(HMY_RPC_URL)
-const InputDataDecoder = require('ethereum-input-data-decoder');
-const erc20 = require('../abi/erc20.json')
-const perBlock = 2 // 2 seconds default harmony
+const request = require('request');
+const async = require('async');
 
 function Profile () {
 
+	const getBalance = async (address) => {
+		return new Promise (async (resolve, reject) => {
+			try {
+				let options = {
+					uri: HMY_RPC_URL,
+					method: 'POST',
+					headers: {
+						'Content-Type': 'application/json',
+					},
+					body: JSON.stringify({
+						"id": "1",
+						"jsonrpc": "2.0",
+						"method": "hmyv2_getBalance",
+						"params": [
+							address,
+						]
+					}),
+				}
+
+				request(options, async (err, response, body) => {
+					if (err) return reject({status : 500, message : 'Internal Server Error'});
+					let hasil = JSON.parse(body);
+					let balance = hasil.result/1e18;
+					return resolve({status : 200, balance : balance});
+				});
+			} catch (err) {
+				return reject({status : 500, message : err.message});
+			}
+		});
+	}
+
+	const getLastTransactions = async (address) => {
+		return new Promise(async (resolve, reject) => {
+			try {
+				let options = {
+					uri: HMY_RPC_URL,
+					method: 'POST',
+					headers: {
+						'Content-Type': 'application/json',
+					},
+					body: JSON.stringify({
+						"id": "1",
+						"jsonrpc": "2.0",
+						"method": "hmyv2_getBalance",
+						"params": [
+							address,
+						]
+					}),
+				}
+			} catch (err) {
+				return reject(err);
+			}
+		})
+	}
+
 	this.address = async (req, res, next) => {
-		let address = req.params.address
-		return res.render('profile',{
-			title : `Defi Kingdoms - Address ${address}`,
-			address : address,
+		let address = req.params.address;
+
+		async.parallel({
+			balance : function (callback) {
+				getBalance(address)
+					.then((rows) => {
+						return callback(null, rows.
+							balance);
+					})
+					.catch((err) => {
+						return callback(err, null);
+					})
+			}
+		}, (err, results) => {
+			if (err) return next(err);
+			console.log('[RESULTS] ', results);
+			return res.render('profile',{
+				title : `Defi Kingdoms - Address ${address}`,
+				address : address,
+				balance : results.balance,
+			})
 		})
 	}
 }
